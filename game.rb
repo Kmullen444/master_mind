1cc62f04c3d52e733a3a5d4a614af2bc14a43699
Kevin <kmullen444@gmail.com>
2021-11-12 18:41:00 -0800

    Fixed bug where color matches weren't being counted correctly

--- game.rb
+++ game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_reader :code
+  attr_reader :code, :COLORS
   @@COLORS = [:R,:O,:Y,:G,:B,:I]
 
   def initialize(players = 1)
@@ -18,27 +18,32 @@ class Game
         player_guess.all? { |color| @@COLORS.include?(color) }
   end
 
-  def correct_peg (player_guess)
+  def exact_match(player_guess)
     count = 0
     code.each_with_index do |color1, idx1|
       player_guess.each_with_index do |color2, idx2|
-        count += 1 if color1 == color2 && idx1 == idx2
+       count += 1 if color1 == color2 && idx1 == idx2 
       end
-    end
-    count
+    end   
+   count
   end
 
-  # TODO: Needs to only count pegs from guess if the peg is included but
-  # isn't a perfect matach
-
-  def correct_color (player_guess)
-     count = 0
-    code.each_with_index do |color1, idx1|
-      player_guess.each_with_index do |color2, idx2|
-        count += 1 if code.include?(color2) && color1 != color2
-      end
+  def match_remover(code_to_check, main_code)
+    code_to_check.select.with_index do |color, i|
+      color if code_to_check[i] != main_code[i]
     end
-    count
-
   end
+
+  def color_match(player_guess)
+    no_match_code = match_remover(code, player_guess)
+    no_match_player = match_remover(player_guess, code)
+    no_match_player.count { |color| no_match_code.include?(color) }
+  end 
+
+  def win?(player_guess)
+    player_guess == code
+  end
+
+  # TODO: Make prompt for a with color_match and exact_match
+  # Make a turn the will loop until win?
 end
